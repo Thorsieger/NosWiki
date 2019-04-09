e862141cd9cc26a0a9d30871cf269c53f7104f4f
Thorsieger <mertz.thomas@live.fr>
2019-04-09 14:59:42 +0200

    gestion des parfumes

---
+++ parfume/parfume.js
@@ -0,0 +1,21 @@
+function calculParfume()
+{
+    var itemLVL = document.getElementById("itemLVL").value;
+    var rareLVL = document.getElementById("rareLVL").value;
+
+    var tabParfume = [
+        [0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0],
+        [0,0,0,0,0,5,10],
+        [0,0,5,5,10,20,40],
+        [0,5,10,10,20,40,80],
+        [5,10,10,20,40,80,120],
+        [10,10,20,40,80,120,160],
+        [10,20,40,80,120,160,200],
+        [20,40,80,120,160,200,300]
+    ];
+
+    var coutParfume = tabParfume[parseInt((itemLVL)/10)-1][rareLVL-1];
+    document.getElementById("nbrParfume").value = coutParfume;
+
+}
